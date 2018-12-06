ec87d52530b24dcf5b3283c0e4d8cb3c2c044f0e
Stephanie <stephanie.holcomb@gmail.com>
2018-12-06 06:18:40 -0800

    Day 5 part 2 super slow but it works

--- Day05/day05_Alchemical_Reduction.py
+++ Day05/day05_Alchemical_Reduction.py
@@ -35,12 +35,15 @@ def check(new_string,flg):
     return flg    
 
 def main():
+    """
+    # PART I:
 
     new_string = ""
     flg = False
     #pstring = "dabAcCaCBAcCcaDA" 
 
     with open('day05_input.txt','r') as input_file:
+    #with open('day05_testinput.txt','r') as input_file:
         pstring = input_file.read().rstrip('\r\n')
            
     while not flg:
@@ -51,7 +54,39 @@ def main():
         flg = check(pstring, flg)
         #print(flg)
 
-    print(pstring, "length",len(pstring))
+    #print(pstring, "length",len(pstring))
+    print("length of polymer:", len(pstring))
+    """
+    
+
+    # PART II:
+
+    temp = ""
+    flg = False
+
+    with open('day05_input.txt','r') as input_file:
+    #with open('day05_testinput.txt','r') as input_file:
+        pstring = input_file.read().rstrip('\r\n')
+
+    min_length = len(pstring)
+
+    for letter in range(ord('a'), ord('z') + 1):
+        #print(chr(letter))
+        flg = False
+        temp = pstring[:]
+
+        temp = temp.replace(chr(letter),'')
+        temp = temp.replace(chr(letter).swapcase(), '')
+
+        while not flg:
+            temp = compare(temp)
+            flg = check(temp, flg)
+            tlength = len(temp)
+            min_length = min(tlength, min_length)
+        
+ 
+    print("Length of shortest polymer:", min_length)
+
 
 if __name__ == "__main__":
     main()
